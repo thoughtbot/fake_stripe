b3128a690e7d9790f960badf382c6d9e05551be0
Dan Barber <github@danbarber.me>
2018-10-25 11:00:08 -0400

    Address review comments

--- lib/fake_stripe/stub_stripe_js.rb
+++ lib/fake_stripe/stub_stripe_js.rb
@@ -1,42 +1,42 @@
-require 'capybara'
-require 'capybara/server'
-require 'fake_stripe/utils'
-require 'sinatra/base'
+require "capybara"
+require "capybara/server"
+require "fake_stripe/utils"
+require "sinatra/base"
 
 module FakeStripe
   class StubStripeJS < Sinatra::Base
-    get '/v1/' do
-      file_path = File.join(File.dirname(__FILE__), '/assets/v1.js')
+    get "/v1/" do
+      file_path = File.join(File.dirname(__FILE__), "/assets/v1.js")
 
-      content_type 'text/javascript'
+      content_type "text/javascript"
       status 200
       IO.read(file_path)
     end
 
-    get '/v2/' do
-      file_path = File.join(File.dirname(__FILE__), '/assets/v2.js')
-      mock_file_path = File.join(File.dirname(__FILE__), '/assets/v2-mock.js')
+    get "/v2/" do
+      file_path = File.join(File.dirname(__FILE__), "/assets/v2.js")
+      mock_file_path = File.join(File.dirname(__FILE__), "/assets/v2-mock.js")
 
-      content_type 'text/javascript'
+      content_type "text/javascript"
       status 200
       IO.read(file_path) + IO.read(mock_file_path)
     end
 
-    get '/v3/' do
-      file_path = File.join(File.dirname(__FILE__), '/assets/v3.js')
+    get "/v3/" do
+      file_path = File.join(File.dirname(__FILE__), "/assets/v3.js")
 
-      content_type 'text/javascript'
+      content_type "text/javascript"
       status 200
       IO.read(file_path)
     end
 
     def self.boot(port = FakeStripe::Utils.find_available_port)
       instance = new
-      Capybara::Server.new(instance, port: port).tap { |server| server.boot }
+      Capybara::Server.new(instance, port: port).tap(&:boot)
     end
 
     def self.boot_once
-      @@stripe_js_server ||= FakeStripe::StubStripeJS.boot(self.server_port)
+      @@stripe_js_server ||= FakeStripe::StubStripeJS.boot(server_port)
     end
 
     def self.server_port
