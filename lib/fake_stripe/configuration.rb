81b26bcd262aa56f0c469488e10c416c532c83d0
OKURA Masafumi <masafumi.o1988@gmail.com>
2019-08-15 17:02:00 +0900

    Use `||=` instead of `or` to suppress warning
    The warning tells us the instance variable is not initialized.

--- lib/fake_stripe/configuration.rb
+++ lib/fake_stripe/configuration.rb
@@ -5,7 +5,7 @@ module FakeStripe
     DEFAULT_FIXTURE_PATH = File.join(File.dirname(__FILE__), 'fixtures/')
 
     def fixture_path
-      @fixture_path or DEFAULT_FIXTURE_PATH
+      @fixture_path ||= DEFAULT_FIXTURE_PATH
     end
 
     def configure
