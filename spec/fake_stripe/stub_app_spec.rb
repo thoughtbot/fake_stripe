93c26a85b24a512cab947efacd6fb9540135827d
Mike Menne <mike@humanagency.com>
2021-02-12 12:03:15 -0600

    Removing deprecated methods

--- spec/fake_stripe/stub_app_spec.rb
+++ spec/fake_stripe/stub_app_spec.rb
@@ -81,20 +81,6 @@ describe FakeStripe::StubApp do
     end
   end
 
-  describe 'POST /v1/charges/:charge_id/refund' do
-    it 'returns a fake refund response' do
-      result = Stripe::Charge.retrieve('ABC123').refund
-
-      expect(result.refunded).to eq true
-    end
-
-    it 'increments the refund counter' do
-      expect do
-        Stripe::Charge.retrieve('ABC123').refund
-      end.to change(FakeStripe, :refund_count).by(1)
-    end
-  end
-
   describe "POST /v1/customers" do
     it "increments the customer counter" do
       expect do
